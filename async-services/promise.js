ebf3bcd6d02fdc4d2b32bffd83ea74cbbb81fd5e
Johan Picq <Johan.picq@dailymotion.com>
2022-12-26 12:02:19 +0100

    promise's services

--- async-services/promise.js
+++ async-services/promise.js
@@ -1,6 +1,34 @@
 import { services, getTimer, initTimer } from "./init.js";
 
 export const initPromiseServices = () => {
+  const startServiceWithPromise = (service, duration) => {
+    return new Promise((resolve) => {
+      let nextServiceToWork = null;
+      services.forEach((s) => {
+        if (s.dependancies === service) {
+          nextServiceToWork = s;
+        }
+      });
+      console.log("#", service, "started at", getTimer(), "ms");
+      setTimeout(() => {
+        console.log("#", service, "ended at", getTimer(), "ms");
+        resolve(nextServiceToWork);
+      }, duration);
+    });
+  };
   initTimer();
-  console.log("promise");
+  services.forEach((service) => {
+    if (service.dependancies === null) {
+      startServiceWithPromise(service.id, service.duration).then(
+        (nextServiceToWork) => {
+          if (nextServiceToWork) {
+            startServiceWithPromise(
+              nextServiceToWork.id,
+              nextServiceToWork.duration
+            );
+          }
+        }
+      );
+    }
+  });
 };
